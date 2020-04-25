e18621e82e0f478602d3bb15cdec07fbf4fd46ff
Kounty_Rolen <molodcovdaniil123@gmail.com>
2020-04-25 10:20:37 +0300

    addClass
    Был добавлен класс описывающий сущность города и 2 метода, для конвертации обьекта в строку и для сортировки

---
+++ Exam/Clases/City.cs
@@ -0,0 +1,37 @@
+﻿//Описать тип City для хранения данных о городе (название, дата основания, количество жителей).
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Exam.Clases
+{
+
+    class City : Object
+    {
+        private string name;
+        private DateTime date;
+        private Int64 count;//Думаю что Int64 хватит
+
+        public string Name { get => name; set => name = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public Int64 Count { get => count; set => count = value; }
+
+        public City(string name, DateTime date, Int64 count)
+        {
+            this.Name = name;
+            this.Date = date;
+            this.Count = count;
+        }
+        public City()
+        {
+            this.Name = "";
+            this.Date = DateTime.MinValue;
+            this.Count = 0;
+        }
+        public override string ToString()
+        {
+            return "Название города:" + Name + "\nДата основания:" + Date.ToShortDateString() + "\nНаселение:" + Count + "\n";
+        }
+
+    }
+}
---
+++ Exam/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exam.Clases;
+
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите кол-во городов для заполнения:");
+                var CityCount = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\n");
+                City[] cities = new City[CityCount];
+                for (int i = 0; i >= CityCount; i++)
+                {
+                    Console.WriteLine("Введите имя города:");
+                    cities[i].Name = Console.ReadLine();
+                    Console.WriteLine("Введите кол-во жителей:");
+                    Int64 count;
+                    Int64.TryParse(Console.ReadLine(), out count);
+                    cities[i].Count = count;
+                    Console.WriteLine("Введите Дата основания города(YYYY.MM.DD):");
+                    string date = Console.ReadLine();
+                    DateTime result;
+                    DateTime.TryParse(date, out result);
+                    cities[i].Date = result;
+                }
+                cities = cities.AsQueryable<City>().OrderBy(c => c.Name).ThenBy(c => c.Date).ToArray();
+                foreach (var city in cities)
+                {
+                    Console.WriteLine(cities.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "\n" + ex.StackTrace);
+            }
+
+        }
+    }
+}
